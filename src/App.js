85b80ce70973e25dabd0354b245aa82e537d9dbe
ZombieKid05 <nair.athul26@gmail.com>
2023-01-13 20:45:49 +0530

    updated the logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.css';
 const App = () => {
   return (
     <div className="app-container">
-      <img className="logo" src="logo.svg" alt="Logo" />
+      <img className="logo" src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" alt="Logo" />
       <a className="link" href="https://www.propelleraero.com/dirtmate/">Visit website</a>
     </div>
   );

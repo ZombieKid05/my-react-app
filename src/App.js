a6fbb5e77e101fa058a96e2422b3c80e6df317b4
ZombieKid05 <nair.athul26@gmail.com>
2023-01-13 20:24:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <img className="logo" src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" alt="Logo" />
+      <a className="link" href="https://www.propelleraero.com/dirtmate/">Visit website</a>
+    </div>
+  );
+};
+
+export default App;
